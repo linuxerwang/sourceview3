58bbfa485ab5b0bccc498998d59a35fa1a84d3f9
Linuxer Wang <linuxerwang@gmail.com>
2020-05-23 10:01:51 -0700

    Add more bindings: SourceStyle, SourceStyleScheme, SourceStyleSchemeManager, SourceStyleSchemeChooser, SourceStyleSchemeChooserButton, SourceStyleSchemeChooserWidget.

--- sourceview.go
+++ sourceview.go
@@ -5,6 +5,12 @@ package sourceview
 // #include <gtksourceview/gtksourcegutter.h>
 // #include <gtksourceview/gtksourcelanguage.h>
 // #include <gtksourceview/gtksourcelanguagemanager.h>
+// #include <gtksourceview/gtksourcestyle.h>
+// #include <gtksourceview/gtksourcestylescheme.h>
+// #include <gtksourceview/gtksourcestyleschemechooser.h>
+// #include <gtksourceview/gtksourcestyleschemechooserbutton.h>
+// #include <gtksourceview/gtksourcestyleschemechooserwidget.h>
+// #include <gtksourceview/gtksourcestyleschememanager.h>
 // #include <gtksourceview/gtksourceview.h>
 // #include "sourceview.go.h"
 import "C"
@@ -24,6 +30,12 @@ func init() {
 		{glib.Type(C.gtk_source_gutter_get_type()), marshalSourceGutter},
 		{glib.Type(C.gtk_source_language_get_type()), marshalSourceLanguage},
 		{glib.Type(C.gtk_source_language_manager_get_type()), marshalSourceLanguageManager},
+		{glib.Type(C.gtk_source_style_get_type()), marshalSourceStyle},
+		{glib.Type(C.gtk_source_style_scheme_get_type()), marshalSourceStyleScheme},
+		{glib.Type(C.gtk_source_style_scheme_chooser_get_type()), marshalSourceStyleSchemeChooser},
+		{glib.Type(C.gtk_source_style_scheme_chooser_button_get_type()), marshalSourceStyleSchemeChooserButton},
+		{glib.Type(C.gtk_source_style_scheme_chooser_widget_get_type()), marshalSourceStyleSchemeChooserWidget},
+		{glib.Type(C.gtk_source_style_scheme_manager_get_type()), marshalSourceStyleSchemeManager},
 		{glib.Type(C.gtk_source_view_get_type()), marshalSourceView},
 	}
 	glib.RegisterGValueMarshalers(tm)
@@ -33,6 +45,12 @@ func init() {
 	gtk.WrapMap["GtkSourceGutter"] = wrapSourceGutter
 	gtk.WrapMap["GtkSourceLanguage"] = wrapSourceLanguage
 	gtk.WrapMap["GtkSourceLanguageManager"] = wrapSourceLanguageManager
+	gtk.WrapMap["GtkSourceStyle"] = wrapSourceStyle
+	gtk.WrapMap["GtkSourceStyleScheme"] = wrapSourceStyleScheme
+	gtk.WrapMap["GtkSourceStyleSchemeChooser"] = wrapSourceStyleSchemeChooser
+	gtk.WrapMap["GtkSourceStyleSchemeChooserButton"] = wrapSourceStyleSchemeChooserButton
+	gtk.WrapMap["GtkSourceStyleSchemeChooserWidget"] = wrapSourceStyleSchemeChooserWidget
+	gtk.WrapMap["GtkSourceStyleSchemeManager"] = wrapSourceStyleSchemeManager
 }
 
 func gbool(b bool) C.gboolean {
@@ -40,6 +58,10 @@ func gbool(b bool) C.gboolean {
 		return C.gboolean(1)
 	}
 	return C.gboolean(0)
+}
+
+func goString(cstr *C.gchar) string {
+	return C.GoString((*C.char)(cstr))
 }
 
 /*
@@ -249,6 +271,11 @@ func (v *SourceBuffer) SetMaxUndoLevels(levels int) {
 	C.gtk_source_buffer_set_max_undo_levels(v.native(), C.gint(levels))
 }
 
+// SetStyleScheme is a wrapper around gtk_source_buffer_set_style_scheme().
+func (v *SourceBuffer) SetStyleScheme(scheme *SourceStyleScheme) {
+	C.gtk_source_buffer_set_style_scheme(v.native(), scheme.native())
+}
+
 /*
  * GtkSourceLanguageManager
  */
@@ -336,4 +363,437 @@ func marshalSourceLanguage(p uintptr) (interface{}, error) {
 
 func wrapSourceLanguage(obj *glib.Object) *SourceLanguage {
 	return &SourceLanguage{obj}
+}
+
+/*
+ * GtkSourceStyle
+ */
+
+// SourceStyle is a representation of GtkSourceStyle.
+type SourceStyle struct {
+	*glib.Object
+}
+
+// native returns a pointer to the underlying GtkSourceStyle.
+func (v *SourceStyle) native() *C.GtkSourceStyle {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkSourceStyle(p)
+}
+
+func marshalSourceStyle(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapSourceStyle(obj), nil
+}
+
+func wrapSourceStyle(obj *glib.Object) *SourceStyle {
+	return &SourceStyle{obj}
+}
+
+// Copy is a wrapper around gtk_source_style_copy().
+func (v *SourceStyle) Copy() (*SourceStyle, error) {
+	c := C.gtk_source_style_copy(v.native())
+	if c == nil {
+		return nil, errNilPtr
+	}
+	return wrapSourceStyle(glib.Take(unsafe.Pointer(c))), nil
+}
+
+// Apply is a wrapper around gtk_source_style_apply().
+func (v *SourceStyle) Apply(tag *gtk.TextTag) {
+	ctag := C.toGtkTextTag(unsafe.Pointer(tag.GObject))
+	C.gtk_source_style_apply(v.native(), ctag)
+}
+
+/*
+ * GtkSourceStyleScheme
+ */
+
+// SourceStyleScheme is a representation of GtkSourceStyleScheme.
+type SourceStyleScheme struct {
+	*glib.Object
+}
+
+// native returns a pointer to the underlying GtkSourceStyleScheme.
+func (v *SourceStyleScheme) native() *C.GtkSourceStyleScheme {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkSourceStyleScheme(p)
+}
+
+func marshalSourceStyleScheme(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapSourceStyleScheme(obj), nil
+}
+
+func wrapSourceStyleScheme(obj *glib.Object) *SourceStyleScheme {
+	return &SourceStyleScheme{obj}
+}
+
+// GetID is a wrapper around gtk_source_style_scheme_get_id().
+func (v *SourceStyleScheme) GetID() (string, error) {
+	c := C.gtk_source_style_scheme_get_id(v.native())
+	if c == nil {
+		return "", errNilPtr
+	}
+	gostr := goString(c)
+	C.g_free(C.gpointer(c))
+	return gostr, nil
+}
+
+// GetName is a wrapper around gtk_source_style_scheme_get_name().
+func (v *SourceStyleScheme) GetName() (string, error) {
+	c := C.gtk_source_style_scheme_get_name(v.native())
+	if c == nil {
+		return "", errNilPtr
+	}
+	gostr := goString(c)
+	C.g_free(C.gpointer(c))
+	return gostr, nil
+}
+
+// GetDescription is a wrapper around gtk_source_style_scheme_get_description().
+func (v *SourceStyleScheme) GetDescription() (string, error) {
+	c := C.gtk_source_style_scheme_get_description(v.native())
+	if c == nil {
+		return "", errNilPtr
+	}
+	gostr := goString(c)
+	C.g_free(C.gpointer(c))
+	return gostr, nil
+}
+
+// GetAuthors is a wrapper around gtk_source_style_scheme_get_authors().
+func (v *SourceStyleScheme) GetAuthors() []string {
+	var authors []string
+	cauthors := C.gtk_source_style_scheme_get_authors(v.native())
+	if cauthors == nil {
+		return nil
+	}
+	for {
+		if *cauthors == nil {
+			break
+		}
+		authors = append(authors, C.GoString((*C.char)(*cauthors)))
+		cauthors = C.next_gcharptr(cauthors)
+	}
+	return authors
+}
+
+// GetFileName is a wrapper around gtk_source_style_scheme_get_filename().
+func (v *SourceStyleScheme) GetFileName() (string, error) {
+	c := C.gtk_source_style_scheme_get_filename(v.native())
+	if c == nil {
+		return "", errNilPtr
+	}
+	gostr := goString(c)
+	C.g_free(C.gpointer(c))
+	return gostr, nil
+}
+
+// GetStyle is a wrapper around gtk_source_style_scheme_get_style().
+func (v *SourceStyleScheme) GetStyle(id string) (*SourceStyle, error) {
+	cstr1 := (*C.gchar)(C.CString(id))
+	defer C.free(unsafe.Pointer(cstr1))
+
+	c := C.gtk_source_style_scheme_get_style(v.native(), cstr1)
+	if c == nil {
+		return nil, errNilPtr
+	}
+	return wrapSourceStyle(glib.Take(unsafe.Pointer(c))), nil
+}
+
+/*
+ * GtkSourceStyleSchemeManager
+ */
+
+// SourceStyleSchemeManager is a representation of GtkSourceStyleSchemeManager.
+type SourceStyleSchemeManager struct {
+	*glib.Object
+}
+
+// native returns a pointer to the underlying GtkSourceStyleSchemeManager.
+func (v *SourceStyleSchemeManager) native() *C.GtkSourceStyleSchemeManager {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkSourceStyleSchemeManager(p)
+}
+
+func marshalSourceStyleSchemeManager(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapSourceStyleSchemeManager(obj), nil
+}
+
+func wrapSourceStyleSchemeManager(obj *glib.Object) *SourceStyleSchemeManager {
+	return &SourceStyleSchemeManager{obj}
+}
+
+// SourceStyleSchemeManagerNew is a wrapper around gtk_source_style_scheme_manager_new().
+func SourceStyleSchemeManagerNew() (*SourceStyleSchemeManager, error) {
+	c := C.gtk_source_style_scheme_manager_new()
+	if c == nil {
+		return nil, errNilPtr
+	}
+	return wrapSourceStyleSchemeManager(glib.Take(unsafe.Pointer(c))), nil
+}
+
+// SourceStyleSchemeManagerGetDefault is a wrapper around gtk_source_style_scheme_manager_get_default().
+func SourceStyleSchemeManagerGetDefault() (*SourceStyleSchemeManager, error) {
+	c := C.gtk_source_style_scheme_manager_get_default()
+	if c == nil {
+		return nil, errNilPtr
+	}
+	return wrapSourceStyleSchemeManager(glib.Take(unsafe.Pointer(c))), nil
+}
+
+// SetSearchPath is a wrapper around gtk_source_style_scheme_manager_set_search_path().
+func (v *SourceStyleSchemeManager) SetSearchPath(paths []string) {
+	cpaths := C.make_strings(C.int(len(paths) + 1))
+	for i, path := range paths {
+		cstr := C.CString(path)
+		defer C.free(unsafe.Pointer(cstr))
+		C.set_string(cpaths, C.int(i), (*C.gchar)(cstr))
+	}
+
+	C.set_string(cpaths, C.int(len(paths)), nil)
+	C.gtk_source_style_scheme_manager_set_search_path(v.native(), cpaths)
+	C.destroy_strings(cpaths)
+}
+
+// AppendSearchPath is a wrapper around gtk_source_style_scheme_manager_append_search_path().
+func (v *SourceStyleSchemeManager) AppendSearchPath(path string) {
+	cstr := C.CString(path)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_source_style_scheme_manager_append_search_path(v.native(), (*C.gchar)(cstr))
+}
+
+// PrependSearchPath is a wrapper around gtk_source_style_scheme_manager_prepend_search_path().
+func (v *SourceStyleSchemeManager) PrependSearchPath(path string) {
+	cstr := C.CString(path)
+	defer C.free(unsafe.Pointer(cstr))
+	C.gtk_source_style_scheme_manager_prepend_search_path(v.native(), (*C.gchar)(cstr))
+}
+
+// GetSearchPath is a wrapper around gtk_source_style_scheme_manager_get_search_path().
+func (v *SourceStyleSchemeManager) GetSearchPath() []string {
+	var paths []string
+	cpaths := C.gtk_source_style_scheme_manager_get_search_path(v.native())
+	if cpaths == nil {
+		return nil
+	}
+	for {
+		if *cpaths == nil {
+			break
+		}
+		paths = append(paths, C.GoString((*C.char)(*cpaths)))
+		cpaths = C.next_gcharptr(cpaths)
+	}
+	return paths
+}
+
+// GetSchemeIDs is a wrapper around gtk_source_style_scheme_manager_get_scheme_ids().
+func (v *SourceStyleSchemeManager) GetSchemeIDs() []string {
+	var ids []string
+	cids := C.gtk_source_style_scheme_manager_get_scheme_ids(v.native())
+	if cids == nil {
+		return nil
+	}
+	for {
+		if *cids == nil {
+			break
+		}
+		ids = append(ids, C.GoString((*C.char)(*cids)))
+		cids = C.next_gcharptr(cids)
+	}
+	return ids
+}
+
+// GetScheme is a wrapper around gtk_source_style_scheme_manager_get_scheme().
+func (v *SourceStyleSchemeManager) GetScheme(id string) *SourceStyleScheme {
+	cstr1 := (*C.gchar)(C.CString(id))
+	defer C.free(unsafe.Pointer(cstr1))
+
+	c := C.gtk_source_style_scheme_manager_get_scheme(v.native(), cstr1)
+	if c == nil {
+		return nil
+	}
+	return wrapSourceStyleScheme(glib.Take(unsafe.Pointer(c)))
+}
+
+/*
+ * GtkSourceStyleSchemeChooser
+ */
+
+// ISourceStyleSchemeChooser is an interface type implemented by all structs
+// embedding a GtkSourceStyleSchemeChooser.  It is meant to be used as an
+// argument type for wrapper functions that wrap around a C GTK function taking
+// a GtkSourceStyleSchemeChooser.
+type ISourceStyleSchemeChooser interface {
+	toSourceStyleSchemeChooser() *C.GtkSourceStyleSchemeChooser
+}
+
+/*
+ * GtkSourceStyleSchemeChooser
+ */
+
+// SourceStyleSchemeChooser is a representation of GtkSourceView's
+// GtkSourceStyleSchemeChooser GInterface.
+type SourceStyleSchemeChooser struct {
+	*glib.Object
+}
+
+// native returns a pointer to the underlying GObject as a GtkSourceStyleSchemeChooser.
+func (v *SourceStyleSchemeChooser) native() *C.GtkSourceStyleSchemeChooser {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toGtkSourceStyleSchemeChooser(p)
+}
+
+func (v *SourceStyleSchemeChooser) toSourceStyleSchemeChooser() *C.GtkSourceStyleSchemeChooser {
+	if v == nil {
+		return nil
+	}
+	return v.native()
+}
+
+func marshalSourceStyleSchemeChooser(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapSourceStyleSchemeChooser(obj), nil
+}
+
+func wrapSourceStyleSchemeChooser(obj *glib.Object) *SourceStyleSchemeChooser {
+	return &SourceStyleSchemeChooser{obj}
+}
+
+// GetScheme is a wrapper around gtk_source_style_scheme_chooser_get_style_scheme().
+func (v *SourceStyleSchemeChooser) GetScheme() *SourceStyleScheme {
+	c := C.gtk_source_style_scheme_chooser_get_style_scheme(v.native())
+	if c == nil {
+		return nil
+	}
+	return wrapSourceStyleScheme(glib.Take(unsafe.Pointer(c)))
+}
+
+// SetScheme is a wrapper around gtk_source_style_scheme_chooser_set_style_scheme().
+func (v *SourceStyleSchemeChooser) SetScheme(scheme *SourceStyleScheme) {
+	C.gtk_source_style_scheme_chooser_set_style_scheme(v.native(), scheme.native())
+}
+
+/*
+ * GtkSourceStyleSchemeChooserButton
+ */
+
+// SourceStyleSchemeChooserButton is a representation of GtkSourceStyleSchemeChooserButton.
+type SourceStyleSchemeChooserButton struct {
+	gtk.Button
+
+	SourceStyleSchemeChooser
+}
+
+// native returns a pointer to the underlying GtkSourceStyleSchemeChooserButton.
+func (v *SourceStyleSchemeChooserButton) native() *C.GtkSourceStyleSchemeChooserButton {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toSourceStyleSchemeChooserButton(p)
+}
+
+func (v *SourceStyleSchemeChooserButton) toSourceStyleSchemeChooser() *C.GtkSourceStyleSchemeChooser {
+	if v == nil {
+		return nil
+	}
+	return C.toGtkSourceStyleSchemeChooser(unsafe.Pointer(v.GObject))
+}
+
+func marshalSourceStyleSchemeChooserButton(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapSourceStyleSchemeChooserButton(obj), nil
+}
+
+func wrapSourceStyleSchemeChooserButton(obj *glib.Object) *SourceStyleSchemeChooserButton {
+	actionable := &gtk.Actionable{obj}
+	chooser := wrapSourceStyleSchemeChooser(obj)
+	return &SourceStyleSchemeChooserButton{
+		gtk.Button{
+			gtk.Bin{
+				gtk.Container{
+					gtk.Widget{
+						glib.InitiallyUnowned{obj},
+					},
+				},
+			},
+			actionable,
+		},
+		*chooser,
+	}
+}
+
+/*
+ * GtkSourceStyleSchemeChooserWidget
+ */
+
+// SourceStyleSchemeChooserWidget is a representation of GtkSourceStyleSchemeChooserWidget.
+type SourceStyleSchemeChooserWidget struct {
+	gtk.Bin
+
+	SourceStyleSchemeChooser
+}
+
+// native returns a pointer to the underlying GtkSourceStyleSchemeChooserWidget.
+func (v *SourceStyleSchemeChooserWidget) native() *C.GtkSourceStyleSchemeChooserWidget {
+	if v == nil || v.GObject == nil {
+		return nil
+	}
+	p := unsafe.Pointer(v.GObject)
+	return C.toSourceStyleSchemeChooserWidget(p)
+}
+
+func (v *SourceStyleSchemeChooserWidget) toSourceStyleSchemeChooser() *C.GtkSourceStyleSchemeChooser {
+	if v == nil {
+		return nil
+	}
+	return C.toGtkSourceStyleSchemeChooser(unsafe.Pointer(v.GObject))
+}
+
+func marshalSourceStyleSchemeChooserWidget(p uintptr) (interface{}, error) {
+	c := C.g_value_get_object((*C.GValue)(unsafe.Pointer(p)))
+	obj := glib.Take(unsafe.Pointer(c))
+	return wrapSourceStyleSchemeChooserWidget(obj), nil
+}
+
+func wrapSourceStyleSchemeChooserWidget(obj *glib.Object) *SourceStyleSchemeChooserWidget {
+	chooser := wrapSourceStyleSchemeChooser(obj)
+	return &SourceStyleSchemeChooserWidget{
+		gtk.Bin{
+			gtk.Container{
+				gtk.Widget{
+					glib.InitiallyUnowned{obj},
+				},
+			},
+		},
+		*chooser,
+	}
+}
+
+// SourceStyleSchemeChooserWidgetNew is a wrapper around gtk_source_style_scheme_chooser_widget_new().
+func SourceStyleSchemeChooserWidgetNew() (*SourceStyleSchemeChooserWidget, error) {
+	c := C.gtk_source_style_scheme_chooser_widget_new()
+	if c == nil {
+		return nil, errNilPtr
+	}
+	return wrapSourceStyleSchemeChooserWidget(glib.Take(unsafe.Pointer(c))), nil
 }
--- sourceview.go.h
+++ sourceview.go.h
@@ -1,5 +1,23 @@
 #include <gtk/gtk.h>
 
+static inline gchar** make_strings(int count) {
+	return (gchar**)malloc(sizeof(gchar*) * count);
+}
+
+static inline void destroy_strings(gchar** strings) {
+	free(strings);
+}
+
+static inline gchar* get_string(gchar** strings, int n) {
+	return strings[n];
+}
+
+static inline void set_string(gchar** strings, int n, gchar* str) {
+	strings[n] = str;
+}
+
+static inline gchar** next_gcharptr(gchar** s) { return (s+1); }
+
 static GtkTextView *
 toGtkTextView(void *p)
 {
@@ -40,4 +58,46 @@ static GtkSourceLanguage *
 toGtkSourceLanguage(void *p)
 {
 	return (GTK_SOURCE_LANGUAGE(p));
+}
+
+static GtkSourceStyle *
+toGtkSourceStyle(void *p)
+{
+	return (GTK_SOURCE_STYLE(p));
+}
+
+static GtkSourceStyleScheme *
+toGtkSourceStyleScheme(void *p)
+{
+	return (GTK_SOURCE_STYLE_SCHEME(p));
+}
+
+static GtkSourceStyleSchemeManager *
+toGtkSourceStyleSchemeManager(void *p)
+{
+	return (GTK_SOURCE_STYLE_SCHEME_MANAGER(p));
+}
+
+static GtkSourceStyleSchemeChooser *
+toGtkSourceStyleSchemeChooser(void *p)
+{
+	return (GTK_SOURCE_STYLE_SCHEME_CHOOSER(p));
+}
+
+static GtkSourceStyleSchemeChooserButton *
+toSourceStyleSchemeChooserButton(void *p)
+{
+	return (GTK_SOURCE_STYLE_SCHEME_CHOOSER_BUTTON(p));
+}
+
+static GtkSourceStyleSchemeChooserWidget *
+toSourceStyleSchemeChooserWidget(void *p)
+{
+	return (GTK_SOURCE_STYLE_SCHEME_CHOOSER_WIDGET(p));
+}
+
+static GtkTextTag *
+toGtkTextTag(void *p)
+{
+	return (GTK_TEXT_TAG(p));
 }
